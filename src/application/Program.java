ecdbd606d49d9511ac80ba6edc14233d5bc0be64
xandy <>
2025-01-03 09:58:09 -0400

    add: Seller Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
 import models.entities.Department;
+import models.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args){
         Department d = new Department(1, "Books");
-        System.out.println(d);
+        Seller s = new Seller(1, "Bob", "bob@bob.com", new Date(), 1400.00, d);
+        System.out.println(s);
     }
 }
